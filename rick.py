2dd5a3800034295cecd1725cf3969e0bf1a6d521
robot <robot@r3.com>
2016-11-18 01:55:36 +0000

    now contains the good bits

--- rick.py
+++ rick.py
@@ -3,11 +3,7 @@ rick = [
 "Never gonna give you up, never gonna let you down",
 "Never gonna run around and desert you",
 "Never gonna make you cry, never gonna say goodbye"
-"Never gonna tell a lie and hurt you",
-"We've known each other for so long",
-"Your heart's been aching but you're too shy to say it",
-"Inside we both know what's been going on",
-"We know the game and we're gonna play it"]
+"Never gonna tell a lie and hurt you",]
 while True:
 	for i in rick:
     		print(i)
